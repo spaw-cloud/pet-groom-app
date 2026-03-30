1bedb370cb90f2d2f8f601fd17d880e56b239a5e
spaw-cloud <spawcbe@gmail.com>
2026-03-30 13:38:00 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="bg-black text-white px-6 py-4 flex justify-between items-center shadow-md">
-      <h1 className="text-xl font-bold tracking-wide">🐾 SPAW</h1>
+    <div className="bg-black text-white px-6 py-4 flex justify-between items-center">
+      <h1 className="text-lg font-bold">🐾 SPAW</h1>
 
-      <div className="space-x-6 text-sm">
-        <Link to="/" className="hover:text-gray-300">Home</Link>
-        <Link to="/admin/bookings" className="hover:text-gray-300">Admin</Link>
-        <Link to="/login" className="hover:text-gray-300">Login</Link>
+      <div className="space-x-4">
+        <Link to="/">Home</Link>
+        <Link to="/admin/bookings">Admin</Link>
+        <Link to="/login">Login</Link>
       </div>
     </div>
   );
