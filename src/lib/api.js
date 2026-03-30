910fb5d4d4ed98aa96894f35209d518e1e531782
spaw-cloud <spawcbe@gmail.com>
2026-03-30 13:26:43 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,15 +1,1 @@
-import axios from "axios";
-
-const API_BASE = "https://pet-groom-app.onrender.com";
-
-const api = axios.create({
-  baseURL: API_BASE,
-});
-
-// OTP API
-export const sendOtp = (data) => api.post("/api/auth/send-otp", data);
-
-// Services API
-export const getServices = () => api.get("/api/services");
-
-export default api;
+baseURL: "https://pet-groom-app.onrender.com"
